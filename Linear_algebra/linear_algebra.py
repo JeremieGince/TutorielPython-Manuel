d2a92d6b0893d1d7a9211ed11facc51db4e631c6
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2019-12-22 22:23:14 -0500

    Create linear_algebra.py

---
+++ Linear_algebra/linear_algebra.py
@@ -0,0 +1,15 @@
+import sympy as sp
+from sympy.interactive.printing import init_printing
+import numpy as np
+
+
+if __name__ == '__main__':
+    init_printing(use_unicode=False, wrap_line=False)
+    x, y, z, a, b, c = sp.symbols("x, y, z, a, b, c")
+    A = sp.Matrix([[x, y, z], [a, b, c], [x+a, y+b, z+c]])
+    sp.pprint(f"A = {A}")
+
+    A_det = A.det()
+    sp.pprint(f"det(A) = {A_det}")
+    A_inv = A.pinv()
+    sp.pprint(f"A^-1 = {A_inv}")
