4d9618bf98da0804b04953116ab2664b4b822b3d
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-01-03 13:39:44 -0500

    Create functions.py

---
+++ BuiltIn_Functions/functions.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    array: list = [i for i in range(10)]
+    array_sum = sum(array)
+    print(array_sum)
+
+    array_min = min(array)
+    array_max = max(array)
+    print(array_min)
+    print(array_max)
+
+    value = -7
+    value_abs = abs(value)
+    print(value_abs)
